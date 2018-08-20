ba5d54a8c6fb0da6ded2278a04c27ab028e5717b
Jeffrey Cline <jcline@microsoft.com>
2018-08-20 14:05:14 -0700

    Moved the writer flush incase there are more than one package

--- formatter/json-formatter.go
+++ formatter/json-formatter.go
@@ -11,20 +11,20 @@ import (
 // JSONReport writes a JUnit xml representation of the given report to w
 func JSONReport(report *parser.Report, w io.Writer) error {
 
-	for _, testPackage := range report.Packages {
+	writer := bufio.NewWriter(w)
 
+	for _, testPackage := range report.Packages {
 		bytes, err := json.Marshal(testPackage.Tests)
 
 		if err != nil {
 			return err
 		}
 
-		writer := bufio.NewWriter(w)
-
 		writer.Write(bytes)
 		writer.WriteByte('\n')
-		writer.Flush()
 	}
+
+	writer.Flush()
 
 	return nil
 }
